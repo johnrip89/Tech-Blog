6f4b3e5e91d2e06a492f2c838dd2767a7a214e67
johnrip89 <lebronjohn89@gmail.com>
2021-11-20 08:49:32 -0700

    add withAuth to user routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require("../../models");
-
+const withAuth = require('../../utils/auth');
 
 router.get('/', (req, res) => {    
     User.findAll({
@@ -93,7 +93,7 @@ router.post('/login', (req, res) => {
     });
 });
 
-router.post('/logout', (req, res) => {
+router.post('/logout', withAuth, (req, res) => {
     if (req.session.loggedIn) {
         req.session.destroy(() => {
             res.status(204).end();
@@ -124,7 +124,7 @@ router.put('/:id', (req, res) => {
         });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', withAuth, (req, res) => {
     User.destroy({
         where: {
             id: req.params.id
