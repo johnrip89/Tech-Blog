30a7e7d5853cf46c66c8282627d9bb4c1a7b2b9f
johnrip89 <lebronjohn89@gmail.com>
2021-11-20 08:46:35 -0700

    add withAuth to post routes and changed post_url attribute to content

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require("../../models");
-const sequelize = require('../../config/connection');
+const withAuth = require('../../utils/auth');
 
 router.get('/', (req, res) => {
     Post.findAll({
@@ -72,7 +72,7 @@ router.get('/:id', (req, res) => {
         });
 });
 
-router.post('/', (req, res) => {    
+router.post('/', withAuth, (req, res) => {    
     Post.create({
         title: req.body.title,
         content: req.body.content,
@@ -85,10 +85,11 @@ router.post('/', (req, res) => {
         });
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', withAuth, (req, res) => {
     Post.update(
         {
-            title: req.body.title
+            title: req.body.title,
+            content: req.body.content
         },
         {
             where: {
@@ -109,7 +110,7 @@ router.put('/:id', (req, res) => {
         });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', withAuth, (req, res) => {
     Post.destroy({
         where: {
             id: req.params.id
